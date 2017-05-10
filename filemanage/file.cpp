1dcf9d4d9910d944eb2f5eb733c75b1aa7612215
ysmiles <tshu92@gmail.com>
2017-05-09 23:28:00 -0700

    change to cpp

--- filemanage/file.cpp
+++ filemanage/file.cpp
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "errexit.c"
-
 #include "file.h"
 #include <rpc/rpc.h>
 #define RMACHINE "localhost" /* name of remote machine	*/
@@ -12,9 +10,6 @@ CLIENT *handle;              /* handle for remote procedure	*/
 
 int checkfilename(char *fname);
 myres fileread(char *buf);
-
-extern int errno;
-int errexit(const char *format, ...);
 
 #define BUFSIZE 1024
 
@@ -42,58 +37,36 @@ int main(int argc, char *argv[]) {
     while (fgets(filename, sizeof(filename), stdin)) {
         // input abc, filename would be abc\n, sz would be 4
         sz = strlen(filename);
-        filename[outchars - 1] = '\0'; // filename strlen would be 3
+        filename[sz - 1] = '\0'; // filename strlen would be 3
 
-        sz = checkfilename(filename);
+        // also set fpS at server
+        if (sz = checkfilename(filename)) {
+            // file exist
+            myres bufres;
 
-        fp = fopen(filename, "w");
+            strcat(filename, "_copy");
 
-        memset(buf, 0, BUFSIZE);
+            fp = fopen(filename, "w");
+            while (1) {
+                bufres = fileread(filename);
+                sz = bufres.sz;
 
-        puts("before fileread");
-
-        myres bufres;
-        while (1) {
-            bufres = fileread(buf);
-            sz = bufres.sz;
-            strncpy(buf, bufres.content, sz);
-
-            if (sz < 0)
-                errexit("Read failed: %s\n", strerror(errno));
-
-            if (strcmp(buf, "Wrong file name") == 0) {
-                printf("From server: %s\n", buf);
-                sz = 0;
-                // break this while
-                // go to another command line to read filename (outter while)
-                break;
-            } else {
                 printf("get data %d bytes\n", sz);
-                fwrite(buf, sizeof(char), sz, fp);
-                memset(buf, 0, BUFSIZE);
+                fwrite(bufres.content, sizeof(char), sz, fp);
+
+                if (sz < BUFSIZE)
+                    break;
             }
+            fclose(fp);
 
-            if (sz < BUFSIZE) {
-                break;
-            }
-        }
-
-        fclose(fp);
-
-        // open again and check size.
-        fp = fopen(filename, "r");
-
-        fseek(fp, 0, SEEK_END);
-        sz = ftell(fp);
-
-        if (sz > 0) {
+            // open again and check size.
+            fp = fopen(filename, "r");
+            fseek(fp, 0, SEEK_END);
+            sz = ftell(fp);
             printf("%s has been successfully got from server.\n", filename);
             printf("The size is %d bytes.\n", sz);
-        } else {
-            if (remove(filename) != 0)
-                printf("Error: unable to delete the file\n");
-            printf("Failed to get %s from server.\n", filename);
-        }
+        } else
+            printf("failed to get file %s\n", filename);
 
         printf("Input file name: ");
         fflush(stdout);
--- filemanage/file_srp.cpp
+++ filemanage/file_srp.cpp
@@ -3,13 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "errexit.c"
-
 #include "file.h"
 #include <rpc/rpc.h>
-
-extern int errno;
-int errexit(const char *format, ...);
 
 // for server
 static FILE *fpS;
@@ -17,14 +12,13 @@ static FILE *fpS;
 #define BUFSIZE 1024
 
 int checkfilename(char *fname) {
-    puts("Check name start");
-    printf("File %s\n", fname);
+    printf("File %s", fname);
     if (access(fname, R_OK) != -1) {
-        puts("File exits");
+        puts(" exits");
         fpS = fopen(fname, "r");
         return 1;
     } else {
-        puts("File does not exist");
+        puts(" does not exist");
         fpS = NULL;
         return 0;
     }
@@ -44,8 +38,9 @@ myres fileread(char *buf) {
         if ((s = fread(result, sizeof(char), BUFSIZE, fpS)) < BUFSIZE)
             fclose(fpS);
     }
-    puts("It's about to send result back.");
-    myres res; 
+    printf("send data %d bytes\n", s);
+
+    myres res;
     res.sz = s;
     res.content = result;
     return res;
